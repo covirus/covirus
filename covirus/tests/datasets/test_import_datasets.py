70608d5c1d827f2d91c1efa3033dea83383de1c1
Victor Maricato <maricatovictor@gmail.com>
2020-04-08 04:09:15 -0300

    Changing tests to use CountryDataLoader

--- covirus/tests/datasets/test_import_datasets.py
+++ covirus/tests/datasets/test_import_datasets.py
@@ -1,9 +1,23 @@
-from covirus.datasets import load_br
+import pytest
+from covirus.datasets import CountryDataLoader
 import pandas as pd
 import os
-from unittest import mock
 
 
-def test_import_brazillian_dataset(cache_dir):
-    load_br(cache_dir, use_cache=False)
+def test_import_wcota(cache_dir):
+    dataloader = CountryDataLoader()
+    wcota_data = dataloader.load("BR", "wcota")
     assert os.path.exists(cache_dir + "covid19br/" + "cases-brazil-cities.csv")
+    assert isinstance(wcota_data.cities, pd.DataFrame)
+
+
+def test_import_invalid_source(cache_dir):
+    dataloader = CountryDataLoader()
+    with pytest.raises(ValueError) as e:
+        assert dataloader.load("BR", "bond")
+
+
+def test_import_invalid_country(cache_dir):
+    dataloader = CountryDataLoader()
+    with pytest.raises(ValueError) as e:
+        assert dataloader.load("OK")
