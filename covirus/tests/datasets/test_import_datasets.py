6204ec767e8ae05bc65501c4eab6d09b87ee808f
Victor Maricato <maricatovictor@gmail.com>
2020-04-08 02:15:49 -0300

    Test for cache and repo

--- covirus/tests/datasets/test_import_datasets.py
+++ covirus/tests/datasets/test_import_datasets.py
@@ -1,7 +1,9 @@
 from covirus.datasets import load_br
 import pandas as pd
+import os
+from unittest import mock
 
 
-def test_import_brazillian_dataset():
-    dataset = load_br()
-    assert isinstance(dataset, pd.DataFrame)
+def test_import_brazillian_dataset(cache_dir):
+    load_br(cache_dir, use_cache=False)
+    assert os.path.exists(cache_dir + "covid19br/" + "cases-brazil-cities.csv")
