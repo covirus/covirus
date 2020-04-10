33ebeea2b08449f9637dc7cfe901ee4764a91f83
Victor Maricato <maricatovictor@gmail.com>
2020-04-10 20:36:42 -0300

    Refactoring

--- covirus/data/countries/brazil/load_br.py
+++ covirus/data/countries/brazil/load_br.py
@@ -1,5 +1,5 @@
 import logging
-from .sources import (WCotaDataset)
+from .sources import (wcota)
 
 logger = logging.getLogger()
 
@@ -7,7 +7,7 @@ logger = logging.getLogger()
 def load_brazillian_dataset(source):
     logger.info("Loading %s data", source)
     if source == "wcota":
-        dataset = WCotaDataset()
+        dataset = wcota.WCotaDataset()
     else:
         raise ValueError(f"Dataset Could not be Retrived: {source!r}, is it available?")
     return dataset
--- covirus/data/countries/brazil/sources/__init__.py
+++
@@ -1,1 +0,0 @@
-from .wcota import WCotaDataset
