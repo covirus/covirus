7f53978e447839eb14fe2e6632844ce61f5584a6
Victor Maricato <maricatovictor@gmail.com>
2020-04-10 04:19:48 -0300

    Pip Tools to Pip Compile

--- bin/compile-requirements.sh
+++ bin/compile-requirements.sh
@@ -2,5 +2,6 @@ cd $(git rev-parse --show-toplevel)
 set -o pipefail
 set -e
 
+pip install pip-tools
 pip-compile requirements.in --no-emit-trusted-host --no-index
 pip-compile dev-requirements.in --no-emit-trusted-host --no-index
