c3322a2f3e75843e274b8b9e4e1c80e26000324a
Victor Maricato <maricatovictor@gmail.com>
2020-04-11 14:00:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="covirus",
-    version="0.0.4-2",
+    version="0.0.42",
     author="Victor Maricato",
     author_email="maricatovictor@gmail.com",
     description="A library for dealing with COVID19 analysis",
