e105a01dc3eee186c0a48a30a8074fd54d7ae482
Victor Maricato <maricatovictor@gmail.com>
2020-04-11 16:18:22 -0300

    Dataset Functions for Windows

--- covirus/tests/datasets/test_dataset_functions.py
+++ covirus/tests/datasets/test_dataset_functions.py
@@ -1,3 +1,9 @@
 def test_list_dataset_files(wcota_dataset):
-    wcota_dir = "/tmp/covirus/data/datasets/br/covid19br/"
-    assert wcota_dir + "cases-brazil-cities.csv" in wcota_dataset.list_dataset_files()
+    wcota_dir_unix = "/tmp/covirus/data/datasets/br/covid19br/"
+    wcota_dir_win = "/tmp/covirus/data/datasets/br/covid19br\\"
+    filename = "cases-brazil-cities.csv"
+    win_path = wcota_dir_win + filename
+    unix_path = wcota_dir_unix + filename
+
+    files = wcota_dataset.list_dataset_files()
+    assert win_path in files or unix_path in files
