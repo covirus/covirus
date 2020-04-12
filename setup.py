5831f8af1f886bf107755a821768fffd4bffae99
Victor Maricato <maricatovictor@gmail.com>
2020-04-12 02:03:16 -0300

    Refactoring Package Setup and CI

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
-
+from setuptools import setup
 
 setup(
-    name="covirus",
-    version="0.0.43-3",
-    author="Victor Maricato",
-    author_email="maricatovictor@gmail.com",
-    description="A library for dealing with COVID19 analysis",
-    long_description_content_type="text/markdown",
-    url="https://github.com/maricatovictor/covirus",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    setup_requires=['pbr'],
+    pbr=True
 )
