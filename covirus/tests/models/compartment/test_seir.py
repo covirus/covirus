85a97132d0773c947637b717b74a0ac4695bb8ad
Victor Maricato <maricatovictor@gmail.com>
2020-04-13 23:44:52 -0300

    Refactoring Models (#11)
    * Formatting
    
    * Refactoring

--- covirus/tests/models/compartment/test_seir.py
+++ covirus/tests/models/compartment/test_seir.py
@@ -11,12 +11,12 @@ def test_seir_model():
     days = 4
 
     # Run
-    sir_model = SEIR()
-    sir_model.fit(N, E0, I0, R0, beta, gamma, alpha)
-    S, E, I, R = sir_model.predict(days=days)
+    seir_model = SEIR()
+    seir_model.fit(N, E0, I0, R0, beta, gamma, alpha)
+    S, E, I, R = seir_model.predict(days=days)
 
     # Assert
-    assert isinstance(sir_model, CompartmentModel)
+    assert isinstance(seir_model, CompartmentModel)
     assert isinstance(S, pd.Series)
     assert isinstance(E, pd.Series)
     assert isinstance(I, pd.Series)
@@ -26,14 +26,18 @@ def test_seir_model():
     assert is_number(I[0])
     assert is_number(R[0])
 
-    #Values
-    expected_S = [12999846., 12999624.93174401, 12999435.52653244,
-       12999227.08508175]
-    expected_E = [0., 199.29874694, 334.84248468, 463.60144161]
-    expected_I = [152., 110.60586489, 110.34960838, 130.47433841]
-    expected_R = [2.,  65.16364416, 119.28137451, 178.83913824]
+    # Values
+    expected_S = [12999846.0, 12999624.93174401, 12999435.52653244, 12999227.08508175]
+    expected_E = [0.0, 199.29874694, 334.84248468, 463.60144161]
+    expected_I = [152.0, 110.60586489, 110.34960838, 130.47433841]
+    expected_R = [2.0, 65.16364416, 119.28137451, 178.83913824]
 
-    variables_to_validate = [zip(S, expected_S), zip(E, expected_E), zip(I, expected_I), zip(R, expected_R)]
+    variables_to_validate = [
+        zip(S, expected_S),
+        zip(E, expected_E),
+        zip(I, expected_I),
+        zip(R, expected_R),
+    ]
     for variables in variables_to_validate:
         for real, expected in variables:
             real = round(real, 8)
